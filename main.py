dd02ab377dcca1bb60c0bf6d0eac9fd733724b63
João Rodrigo [Development Leadership] <joao_luckow@estudante.sesisenai.org.br>
2024-01-31 17:33:55 -0300

    base: simple webserver with graphs

---
+++ main.py
@@ -0,0 +1,52 @@
+from networktables import NetworkTables
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+from threading import Lock
+from datetime import datetime
+from random import random
+thread = None
+thread_lock = Lock()
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+def get_current_datetime():
+    now = datetime.now()
+    return now.strftime("%m/%d/%Y %H:%M:%S")    
+
+def main():
+    NetworkTables.initialize(server="10.96.11.2")
+    print(NetworkTables._tables)
+
+    app.run()
+    socketio.run(app)
+
+# home
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+def background_thread():
+    print("Generating random sensor values")
+    while True:
+        dummy_sensor_value = round(random() * 100, 3)
+        socketio.emit('updateData', {'value': dummy_sensor_value, "date": get_current_datetime()})
+        socketio.sleep(1)
+
+@socketio.on('connect')
+def connect():
+    global thread
+    print('Client connected')
+
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected',  request.sid)
+
+if __name__ == "__main__":
+    main() 
